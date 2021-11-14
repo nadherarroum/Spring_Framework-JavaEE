72010d4da52c694547c0eed659ac7bcc0229c4a2
Nadher <nadher.arroum@gmail.com>
2021-11-14 22:35:49 +0100

    Constructor Injection Dependent Map

--- src/main/java/com/nadherarroum/spring_frameworkjavaee/Person.java
+++ src/main/java/com/nadherarroum/spring_frameworkjavaee/Person.java
@@ -2,26 +2,37 @@ package com.nadherarroum.spring_frameworkjavaee;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Person {
 
     private int id;
     private String username;
     //private Addresses addresses;
-    private List<String> technology;
+    //private List<String> technology;
+    private Map<String,String> technology;
 
 
     public Person() {
         super();
     }
-/*
+    /*
     public Person(int id, String username, Addresses addresses) {
         this.id = id;
         this.username = username;
         this.addresses = addresses;
     }
- */
-public Person(int id, String username, List<String> technology) {
+    */
+    /*
+    public Person(int id, String username, List<String> technology) {
+        this.id = id;
+        this.username = username;
+        this.technology = technology;
+    }
+     */
+
+public Person(int id, String username, Map<String,String> technology) {
     this.id = id;
     this.username = username;
     this.technology = technology;
@@ -31,9 +42,19 @@ public Person(int id, String username, List<String> technology) {
         System.out.println("- ID : "+id+"\n"+"- Username : "+username);
         //System.out.println(addresses.infoAddress());
 
+        /*
         Iterator<String> it = technology.iterator();
         while (it.hasNext()){
             System.out.println(it.next());
         }
+         */
+
+        Set<Map.Entry<String,String>> set = technology.entrySet();
+        Iterator<Map.Entry<String,String>> it = set.iterator();
+        while (it.hasNext()){
+            Map.Entry<String,String> entry = it.next();
+            System.out.println("> Technology :\t"+entry.getKey()+"\n\t- "+entry.getValue());
+        }
+
     }
 }
