0d5c277b31fcb8e8c79ad9967f816be7fefd136a
Nadher <nadher.arroum@gmail.com>
2021-11-14 22:04:24 +0100

    Constructor Injection Dependent List

--- src/main/java/com/nadherarroum/spring_frameworkjavaee/Person.java
+++ src/main/java/com/nadherarroum/spring_frameworkjavaee/Person.java
@@ -1,25 +1,39 @@
 package com.nadherarroum.spring_frameworkjavaee;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Person {
 
     private int id;
     private String username;
-    private Addresses addresses;
+    //private Addresses addresses;
+    private List<String> technology;
 
 
     public Person() {
         super();
     }
-
+/*
     public Person(int id, String username, Addresses addresses) {
         this.id = id;
         this.username = username;
         this.addresses = addresses;
     }
-
+ */
+public Person(int id, String username, List<String> technology) {
+    this.id = id;
+    this.username = username;
+    this.technology = technology;
+}
 
     public void display(){
         System.out.println("- ID : "+id+"\n"+"- Username : "+username);
-        System.out.println(addresses.infoAddress());
+        //System.out.println(addresses.infoAddress());
+
+        Iterator<String> it = technology.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
     }
 }
