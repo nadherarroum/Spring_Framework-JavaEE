e71de8b00f40377314d51ca0f3fee7833d2a4fc2
Nadher <nadher.arroum@gmail.com>
2021-11-18 22:31:35 +0100

    Setter Getter Dependant Object

--- src/main/java/com/nadherarroum/spring_frameworkjavaee/Addresses.java
+++ src/main/java/com/nadherarroum/spring_frameworkjavaee/Addresses.java
@@ -8,18 +8,49 @@ public class Addresses {
     private String city;
     private String streetNum;
 
-    public Addresses(String phone, String email, String country, String city, String streetNum) {
-        super();
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getStreetNum() {
+        return streetNum;
+    }
+
+    public void setStreetNum(String streetNum) {
         this.streetNum = streetNum;
     }
 
     public String infoAddress(){
         return (
-                "\t> Phone : " + phone +
+                "\n\t> Phone : " + phone +
                 "\n\t> Email :" + email +
                 "\n\t> Country : " + country +
                 "\n\t> City : "+city +
--- src/main/java/com/nadherarroum/spring_frameworkjavaee/Person.java
+++ src/main/java/com/nadherarroum/spring_frameworkjavaee/Person.java
@@ -9,10 +9,7 @@ public class Person {
 
     private int id;
     private String username;
-    private String job;
-    //private Addresses addresses;
-    //private List<String> technology;
-    //private Map<String,String> technology;
+    private Addresses addresses;
 
     public int getId() {
         return id;
@@ -30,68 +27,18 @@ public class Person {
         this.username = username;
     }
 
-    public String getJob() {
-        return job;
+    public Addresses getAddresses() {
+        return addresses;
     }
 
-    public void setJob(String job) {
-        this.job = job;
-    }
-    public void display() {
-        System.out.println("- ID : " + id + "\n" + "- Username : " + username + "\n- Job : "+job);
-    }
-
-
-/*
-    public Person() {
-        super();
-    }
-
-    public Person(int id, String username) {
-        this.id = id;
-        this.username = username;
-    }
-
-    public Person(int id, String username, Addresses addresses) {
-        this.id = id;
-        this.username = username;
+    public void setAddresses(Addresses addresses) {
         this.addresses = addresses;
     }
-*/
-    /*
-    public Person(int id, String username, List<String> technology) {
-        this.id = id;
-        this.username = username;
-        this.technology = technology;
-    }
-     */
 
-    /*
-    public Person(int id, String username, Map<String,String> technology) {
-        this.id = id;
-        this.username = username;
-        this.technology = technology;
+    public void display() {
+        System.out.println("- ID : " + id +
+                "\n" + "- Username : " + username +
+                "\n- Addresses : "+addresses.infoAddress());
     }
-     */
-    /*
-    public void display(){
-        System.out.println("- ID : "+id+"\n"+"- Username : "+username);
-        System.out.println("- Address : \n"+addresses.infoAddress());
-    */
-        /*
-        Iterator<String> it = technology.iterator();
-        while (it.hasNext()){
-            System.out.println(it.next());
-        }
-         */
 
-        /*
-        Set<Map.Entry<String,String>> set = technology.entrySet();
-        Iterator<Map.Entry<String,String>> it = set.iterator();
-        while (it.hasNext()){
-            Map.Entry<String,String> entry = it.next();
-            System.out.println("> Technology :\t"+entry.getKey()+"\n\t- "+entry.getValue());
-        }
-    }
-    */
 }
